ea0c14223bac41db08dae2362c2ed7d3ae69c533
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2024-01-28 02:02:11 -0300

    Increasing project page title margin

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ import { ProjectsCards } from '@/components/projectsCards'
 
 export default function Projects() {
   return (
-    <Area className="py-10">
+    <Area className="py-14">
       <h1 className="text-3xl font-extrabold pb-2">Projetos</h1>
       <h3 className="text-zinc-400 font-bold">
         \\ Freelances e projetos pessoais que já desenvolvi
