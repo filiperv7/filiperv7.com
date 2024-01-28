ac586889a876b28bfde2b21149662ff46b31153e
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2024-01-28 00:40:19 -0300

    Changing alertInfo title

--- src/components/applications/alertInfo.tsx
+++ src/components/applications/alertInfo.tsx
@@ -19,7 +19,7 @@ export function AlertInfo() {
         </Button>
       </HoverCardTrigger>
       <HoverCardContent className="max-w-[22.5rem] p-4 border border-zinc-500 rounded-md bg-zinc-900 opacity-95 max-sm:ml-5 leading-5">
-        <p>Eu sei! Não se assuste!</p>
+        <p className="italic text-sm mb-1">Eu sei! Deixa eu explicar!</p>
         <p>
           Mas, a verdade é que boa parte dos Devs hoje em dia não tem essa base
           bem sólida, isso os torna muito dependentes de ajudas externas e nunca
